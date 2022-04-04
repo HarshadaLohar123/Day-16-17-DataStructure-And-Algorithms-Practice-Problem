157a9f607ee7112ca6845e5df01f64555bde6bee
harshadalohar123 <harshalohar123@gmail.com>
2022-04-04 19:35:01 +0530

    [Harshada]Added: Insertion sort File added.

--- AlgorithmProblems/AlgorithmProblems/Program.cs
+++ AlgorithmProblems/AlgorithmProblems/Program.cs
@@ -2,44 +2,34 @@
 
 namespace AlgorithmProblems
 {
-    internal class Program
+    public class InsertionSort
     {
         static void Main(string[] args)
         {
-            // Declare the variables
-            int a, b, i, j, flag;
-
-            Console.WriteLine("Enter First Value: ");
-            a = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("\nEnter Second Value: ");
-            b = int.Parse(Console.ReadLine());
-
-            // Print display message
-            Console.WriteLine("\nPrime numbers between " +
-                              "{0} and {1} are: ", a, b);
-
-            for (i = a; i <= b; i++)
+            string[] names = { "Banana", "Apple", "Kiwi", "Pineapple", "Mango" };
+            InsertSort(names);
+            foreach (var item in names)
             {
-                if (i == 1 || i == 0)
-                    continue;
-                flag = 1;
-
-                for (j = 2; j <= i / 2; ++j)
-                {
-                    if (i % j == 0)
-                    {
-                        flag = 0;
-                        break;
-                    }
-                }
-
-                // flag = 1 means i is prime
-                // and flag = 0 means i is not prime
-                if (flag == 1)
-                    Console.WriteLine(i);
+                Console.WriteLine(item);
             }
-
+        }
+        public static void InsertSort(IComparable[] array)
+        {
+            int i, j;
+            for (i = 1; i < array.Length; i++)
+            {
+                IComparable value = array[i];
+                j = i - 1;
+                while ((j >= 0) && (array[j].CompareTo(value) > 0))
+                {
+                    array[j + 1] = array[j];
+                    j--;
+                }
+                array[j + 1] = value;
+            }
         }
     }
 }
+
+
+   
