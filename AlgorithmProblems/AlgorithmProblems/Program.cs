ffee8110dde6749034652a408ed918608b6d0c0b
harshadalohar123 <harshalohar123@gmail.com>
2022-04-04 21:16:26 +0530

    [Harshada]Added: String is palindrome or not

--- AlgorithmProblems/AlgorithmProblems/Program.cs
+++ AlgorithmProblems/AlgorithmProblems/Program.cs
@@ -1,39 +1,71 @@
 ﻿using System;
 
-namespace AlgorithmProblems
-{
-    public class BubbleSort
+namespace DataStructureProgram
+{ 
+    public class PalindromeChecker
     {
-        static void bubbleSort(int[] arr)
+        static char[] queue;
+        static int top = -1;
+
+        static void enqueue(char ele)
         {
-            int n = arr.Length;
-            for (int i = 0; i < n - 1; i++)
-                for (int j = 0; j < n - i - 1; j++)
-                    if (arr[j] > arr[j + 1])
-                    {
-                        // swap temp and arr[i]
-                        int temp = arr[j];
-                        arr[j] = arr[j + 1];
-                        arr[j + 1] = temp;
-                    }
+            queue[++top] = ele;
         }
 
-        /* Prints the array */
-        static void printArray(int[] arr)
+        static char dequeue()
         {
-            int n = arr.Length;
-            for (int i = 0; i < n; ++i)
-                Console.Write(arr[i] + " ");
-            Console.WriteLine();
+            return queue[top--];
         }
 
+        // Function that returns 1
+        // if str is a palindrome
+        static int isPalindrome(char[] str)
+        {
+            int length = str.Length;
+
+            queue = new char[length * 4];
+
+            int i, mid = length / 2;
+            for (i = 0; i < mid; i++)
+            {
+                enqueue(str[i]);
+            }
+
+            // Checking if the length of the String
+            // is odd, if odd then neglect the
+            // middle character
+            if (length % 2 != 0)
+            {
+                i++;
+            }
+
+            // While not the end of the String
+            while (i < length)
+            {
+                char ele = dequeue();
+
+                // If the characters differ then the
+                // given String is not a palindrome
+                if (ele != str[i])
+                    return 0;
+                i++;
+            }
+            return 1;
+        }
         static void Main(string[] args)
         {
-            int[] arr = { 32, 45, 67, 2, 56, 78, 1, 54, 7 };
-            bubbleSort(arr);
-            Console.WriteLine("Sorted array");
-            printArray(arr);
+            char[] str = "madam".ToCharArray();
+
+            if (isPalindrome(str) == 1)
+            {
+                Console.Write("True");
+            }
+            else
+            {
+                Console.Write("False");
+            }
         }
     }
 }
+
 
