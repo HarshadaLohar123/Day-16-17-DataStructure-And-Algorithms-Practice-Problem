b53f66d6c8bd19fd2a4baf469f27f2feff59cbc9
harshadalohar123 <harshalohar123@gmail.com>
2022-04-04 20:03:41 +0530

    [Harshada]Added: Bubble sort File added.

--- AlgorithmProblems/AlgorithmProblems/Program.cs
+++ AlgorithmProblems/AlgorithmProblems/Program.cs
@@ -2,34 +2,38 @@
 
 namespace AlgorithmProblems
 {
-    public class InsertionSort
+    public class BubbleSort
     {
+        static void bubbleSort(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n - 1; i++)
+                for (int j = 0; j < n - i - 1; j++)
+                    if (arr[j] > arr[j + 1])
+                    {
+                        // swap temp and arr[i]
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+        }
+
+        /* Prints the array */
+        static void printArray(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n; ++i)
+                Console.Write(arr[i] + " ");
+            Console.WriteLine();
+        }
+
         static void Main(string[] args)
         {
-            string[] names = { "Banana", "Apple", "Kiwi", "Pineapple", "Mango" };
-            InsertSort(names);
-            foreach (var item in names)
-            {
-                Console.WriteLine(item);
-            }
-        }
-        public static void InsertSort(IComparable[] array)
-        {
-            int i, j;
-            for (i = 1; i < array.Length; i++)
-            {
-                IComparable value = array[i];
-                j = i - 1;
-                while ((j >= 0) && (array[j].CompareTo(value) > 0))
-                {
-                    array[j + 1] = array[j];
-                    j--;
-                }
-                array[j + 1] = value;
-            }
+            int[] arr = { 32, 45, 67, 2, 56, 78, 1, 54, 7 };
+            bubbleSort(arr);
+            Console.WriteLine("Sorted array");
+            printArray(arr);
         }
     }
 }
 
-
-   
